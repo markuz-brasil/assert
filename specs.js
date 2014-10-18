39e417b69f2ac4d0399f0c235641df5101174e23
Markuz GJ <markuz.br@gmail.com>
2014-10-18 13:15:17 -0700

    updated packages

--- specs.js
+++ specs.js
@@ -1,3 +1,67 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+"use strict";
+exports.assertRank = assertRank;
+exports.rank = rank;
+exports.rankCheck = rankCheck;
+exports.equalCheck = equalCheck;
+
+var RankToken = function() {
+  var RankToken = function RankToken(base, assert, equal) {
+    this.base = base
+    this.assert = assert
+    this.equal = equal
+
+    this.is = this.is.bind(this)
+    this.isEqual = this.isEqual.bind(this)
+  };
+
+  Object.defineProperties(RankToken.prototype, {
+    is: {
+      writable: true,
+      value: function(value) { return this.assert(this.base, value) }
+    },
+
+    isEqual: {
+      writable: true,
+      value: function(value) { return this.equal(this.base, value) }
+    }
+  });
+
+  return RankToken;
+}();
+
+exports.RankToken = RankToken;
+function assertRank (base) {
+
+  return new RankToken(base, rankCheck, equalCheck)
+}
+
+function rank (value) {
+  // minimal assertions to detect all native ES5 types
+
+  return [
+    (typeof value === 'boolean')|0,
+    (typeof value === 'number')|0,
+    (typeof value === 'string')|0,
+    (typeof value === 'function')|0,
+
+    (value === void 0)|0,
+    (value === null)|0,
+    (value === value)|0,  // 0 if NaN
+
+    (value instanceof Array)|0,
+    (value instanceof RegExp)|0,
+    (value instanceof Date)|0,
+    (value instanceof Error)|0,
+
+  ].join('')
+}
+
+function rankCheck (val0, val1) { return !!(rank(val0) === rank(val1)) }
+function equalCheck (val0, val1) { return !!(val0 === val1) }
+
+},{}],2:[function(require,module,exports){
 "use strict";
 exports.test = test;
 var assertRank = require('./assertRank').assertRank;
@@ -405,3 +469,197 @@ function test () {
 }
 
 test();
+
+},{"./assertRank":1,"./index":3}],3:[function(require,module,exports){
+
+"use strict";
+exports.rank = rank;
+
+var TypeToken = function() {
+  var TypeToken = function TypeToken(type, assertFunc, equalFunc) {
+    if (!(this instanceof TypeToken)) { return new TypeToken(type, assertFunc, equalFunc) }
+    if (isFunction(type) && !isFunction(assertFunc)) { assertFunc = type }
+
+    this.base = type
+    this.assert = isFunction(assertFunc) ? assertFunc : defaultAssert
+    this.equal = isFunction(equalFunc) ? equalFunc : defaultEqual
+
+    this.is = defaultIs.bind(this)
+    this.isnt = defaultIsnt.bind(this)
+    this.isEqual = defaultIsEqual.bind(this)
+  };
+
+  return TypeToken;
+}();
+
+exports.TypeToken = TypeToken;
+function rank (value) {
+  // minimal set of assertions to detect all native ES5 types
+  return [
+    (value === value)|0,  // 0 if NaN
+    (value === void 0)|0,
+    (value === null)|0,
+
+    (typeof value === 'boolean')|0,
+    (typeof value === 'number')|0,
+    (typeof value === 'string')|0,
+    (typeof value === 'function')|0,
+
+    (value instanceof Array)|0,
+    (value instanceof RegExp)|0,
+    (value instanceof Date)|0,
+    (value instanceof Error)|0,
+
+  ].join('')
+}
+
+function ctor (storage) {
+  if (this instanceof ctor) { return ctor(storage) }
+  var ctx = {storage: (storage instanceof Map) ? storage : new Map}
+
+  return {
+    assert: assertToken.bind(ctx),
+    define: defineToken.bind(ctx),
+    storage: ctx.storage,
+  }
+}
+
+var _ref = ctor();
+var assert = _ref.assert;
+var define = _ref.define;
+var storage = _ref.storage;
+exports.assert = assert;
+exports.define = define;
+exports.storage = storage;
+
+assert.define = define;
+assert.storage = storage;
+assert.TypeToken;
+assert.ctor = ctor;
+
+function isFunction (item) { return typeof item === 'function' }
+function isObject (item) { return typeof item === 'object' && item !== null }
+function isString(item) { return typeof item === 'string' }
+var isArray = Array.isArray;
+
+function defaultIs (value) { return !!this.assert(value, this.base) }
+function defaultIsnt (value) { return !this.assert(value, this.base) }
+function defaultIsEqual (value) { return !!this.equal(value, this.base) }
+function defaultEqual (value, base) { return value === base }
+
+function defineToken(type, assertFunc, equalFunc) {
+  if (equalFunc === undefined)
+    equalFunc = defaultEqual;
+
+  if (assertFunc === undefined)
+    assertFunc = defaultAssert;
+
+  if (type instanceof TypeToken) { return type }
+  if (this.storage.has(type)) { return this.storage.get(type)}
+
+  var token = new TypeToken(type, assertFunc, equalFunc);
+  this.storage.set(type, Object.freeze(token));
+  return token;
+}
+
+function assertToken (base) {
+  if (base instanceof TypeToken) { return base }
+  if (this.storage.has(base)) { return this.storage.get(base) }
+
+  return new TypeToken(base, defaultAssert, defaultEqual)
+}
+
+function defaultAssert (value, base) {
+  var isValid, baseIs, valueIs
+  if (value === base) { return true }
+
+  if (base instanceof TypeToken) { baseIs = base.is; base = base.base }
+  if (value instanceof TypeToken) { valueIs = value.is; value = value.base }
+  if (value === base) { return true }
+
+  if (isString(value) && isString(base) && value !== '' && base !== '') {
+    return value === base
+  }
+
+  if (isArray(value) && value.length) {
+    if (listAssert(value, base) === true) { return true}
+    isValid = false
+  }
+
+  if (isArray(base) && base.length) {
+    if (listAssert(base, value) === true) { return true}
+    isValid = false
+  }
+
+  if (!isArray(value) && isObject(value) && Object.keys(value).length && !(value instanceof TypeToken)) {
+    if (structAssert(value, base) === true) { return true }
+    isValid = false
+  }
+
+  if (!isArray(base) && isObject(base) && Object.keys(base).length && !(base instanceof TypeToken)) {
+    if (structAssert(base, value) === true) { return true }
+    isValid = false
+  }
+
+  if (isValid === false) { return false }
+
+  if (isFunction(valueIs)) {
+    isValid = valueIs(base)
+    if (isValid === true) { return true }
+    if (isValid === false) { return false }
+  }
+
+  if (isFunction(value)) {
+    isValid = value(base)
+    if (isValid === true) { return true }
+    if (isValid === false) { return false }
+  }
+
+  if (isFunction(baseIs)) {
+    isValid = baseIs(value)
+    if (isValid === true) { return true }
+    if (isValid === false) { return false }
+  }
+
+  if (isFunction(base)) {
+    isValid = base(value)
+    if (isValid === true) { return true }
+    if (isValid === false) { return false }
+  }
+
+  return rank(base) === rank(value)
+}
+
+function listAssert (typeList, value) {
+  for (var item in typeList) {
+    if (assert(value).is(typeList[item]) === true) {
+      return true
+    }
+  }
+
+  if (isArray(value)) {
+    for (var item in value) {
+      if (assert(value[item]).is(typeList) === true) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+function structAssert (typeStruct, value) {
+  if (!isObject(typeStruct) || !isObject(value)) { return false }
+
+  for (var item in typeStruct) {
+    if (assert(value[item]).is(typeStruct[item]) === false) {
+      return false
+    }
+  }
+
+  return true
+}
+
+
+
+
+},{}]},{},[2])
